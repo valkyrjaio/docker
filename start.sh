32eea7bfa355d3cd0e3bcb2b2881d43e86b159b0
Melech Mizrachi <melechmizrachi@gmail.com>
2017-04-29 14:29:29 -0700

    Activating sync.

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 docker exec valkyrja_docker service php7.1-fpm start
 docker exec valkyrja_docker service nginx restart
 
-#docker exec valkyrja_docker ./var/www/sync-site.sh
-#docker exec valkyrja_docker ./var/www/sync.sh
+docker exec valkyrja_docker ./var/www/sync-site.sh
+docker exec valkyrja_docker ./var/www/sync.sh
