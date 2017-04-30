b4666a3a8252aae944c134444783946778e22005
Melech Mizrachi <melechmizrachi@gmail.com>
2017-04-30 03:44:12 -0700

    Updating autocomplete.

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 docker exec valkyrja_docker service php7.1-fpm start
 docker exec valkyrja_docker service nginx restart
 
-docker exec valkyrja_docker service nginx restart
-docker exec valkyrja_docker service nginx restart
-
 #docker exec valkyrja_docker source ~/.bash_profile
 #docker exec valkyrja_docker . /etc/bash_completion.d/valkyrja
+
+#docker exec valkyrja_docker ./var/www/sync-site.sh
+#docker exec valkyrja_docker ./var/www/sync.sh
